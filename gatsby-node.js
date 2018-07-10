d9fc38b476d91e7ef21e5d5929801d907c0b27da
Alicia Collymore <alicia.collymore@youview.com>
2018-07-10 07:23:46 +0100

    commented out page creating script

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,18 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.onCreatePage = async ({ page, boundActionCreators }) => {
-    const { createPage } = boundActionCreators;
+// exports.onCreatePage = async ({ page, boundActionCreators }) => {
+//     const { createPage } = boundActionCreators;
   
-    return new Promise((resolve, reject) => {
-      if (page.path.match(/^\/pages/)) {
-        // It's assumed that `landingPage.js` exists in the `/layouts/` directory
-        page.layout = "pages";
+//     return new Promise((resolve, reject) => {
+//       if (page.path.match(/^\/pages/)) {
+//         // It's assumed that `landingPage.js` exists in the `/layouts/` directory
+//         page.layout = "pages";
   
-        // Update the page.
-        createPage(page);
-      }
+//         // Update the page.
+//         createPage(page);
+//       }
   
-      resolve();
-    });
-  };
+//       resolve();
+//     });
+//   };
