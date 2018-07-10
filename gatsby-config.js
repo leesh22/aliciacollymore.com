ee75a3935b509020a3c388463e75292cfdc1191b
Alicia Collymore <alicia.collymore@youview.com>
2018-07-10 07:17:20 +0100

    added github pages support

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Alicia Collymore',
   },
+  pathPrefix: "/aliciacollymore.com",
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components'
