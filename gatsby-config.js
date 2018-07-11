505872720b84cfa6912db140de67a2da770f6616
Alicia Collymore <alicia.collymore@youview.com>
2018-07-11 20:38:35 +0100

    fixed path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Alicia Collymore',
   },
-  pathPrefix: "/aliciacollymore.com",
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
